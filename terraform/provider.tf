787fa920aff24a0fc4882864ff9c591ccafea8ca
PG <psgabriel@gmail.com>
2019-07-04 10:57:39 -0300

    terraform provider fix

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,5 @@
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
-
 provider "aws" {
   region = "${var.aws_region}"
   profile = "aws"
-  access_key = "${var.AWS_ACCESS_KEY_ID}"
-  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
   shared_credentials_file = "~/.aws/credentials"
 }
